a8636a953f011419451d1d5f55007ca863fa5742
MityaAndreevich <dmitry.logachev@icloud.com>
2021-11-15 21:35:21 +0300

    Initial Commit

---
+++ GraphicsInUI/GraphicsInUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicsInUIApp.swift
+//  GraphicsInUI
+//
+//  Created by Dmitry Logachev on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct GraphicsInUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
